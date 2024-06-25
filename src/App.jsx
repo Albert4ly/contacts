68b65ca741c3c7d486a9119b61f40cae49997781
Albert <a91614830@gmail.com>
2024-06-25 21:36:03 +0200

    feat:added-NavBar-component-to-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import NavBar from './components/NavBar/NavBar'
 import './App.css'
+
+function ComponentName() {
+  return (
+    <div>
+      <h1>ContactList</h1>
+    </div>
+  )
+}
+
+function ContactListGroupName({groupName}) {
+  return (
+    <div>
+      <h2>{groupName}</h2>
+    </div>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <NavBar />
+      <ComponentName />
+      <ContactListGroupName groupName="BusinessContacts" />
     </>
   )
 }
