09f997cfa2a9698a77ebeaedc26f568a41890f5b
Albert <a91614830@gmail.com>
2024-06-25 21:39:42 +0200

    feat:create-NavBar-component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './NavBar.css'
+
+const NavBar = () => {
+   return (
+      <nav className='navCnt'>
+         <img className='logo__navCnt' src="" alt="" />
+      </nav>
+   )
+}
+
+export default NavBar;
