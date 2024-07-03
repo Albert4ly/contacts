016b87feb8d309913b8fed62c185236669e91c03
Albert <a91614830@gmail.com>
2024-07-03 23:28:55 +0200

    feat:small-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
 import NavBar from './components/NavBar/NavBar'
+import ContactList from './components/ContactList/ContactList'
 import './App.css'
-
-function ComponentName() {
-  return (
-    <div>
-      <h1>ContactList</h1>
-    </div>
-  )
-}
-
-function ContactListGroupName({groupName}) {
-  return (
-    <div>
-      <h2>{groupName}</h2>
-    </div>
-  )
-}
 
 function App() {
 
   return (
     <>
       <NavBar />
-      <ComponentName />
-      <ContactListGroupName groupName="BusinessContacts" />
+      <div className='componentsCnt'>
+        <ContactList />
+      </div>
     </>
   )
 }
