0b40a61c306c8d3b36a4290f1ef7419b78487c60
Albert <a91614830@gmail.com>
2024-07-15 20:40:16 +0200

    refac:change-extension-of-file

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import { ReactDOM } from 'react'
+import NavBar from './components/NavBar/NavBar'
+import { ComponentsCnt } from './containers/ComponentCnt'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <ComponentsCnt />
+    </>
+  )
+}
+
+export default App
