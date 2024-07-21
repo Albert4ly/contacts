e8ee172b33132dc1bb131769cb3de0aac9f73eec
Albert <a91614830@gmail.com>
2024-07-21 23:09:46 +0200

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { ReactDOM } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import { ComponentsCnt } from './containers/ComponentCnt'
-// import "./App.css"
+import "./App.css"
 
 function App() {
 
